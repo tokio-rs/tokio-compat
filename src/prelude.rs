6ebde298f8c28e4e41e80af2ddcd9b0ca02efed5
Taiki Endo <te316e89@gmail.com>
2020-04-21 23:49:54 +0900

    Use doc(no_inline) on unnamed prelude exports (#31)

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,6 @@
 //! A prelude for `tokio` 0.1/0.2 compatibility.
 #[cfg(feature = "sink")]
+#[doc(no_inline)]
 pub use futures_util::compat::Sink01CompatExt as _;
+#[doc(no_inline)]
 pub use futures_util::compat::{Future01CompatExt as _, Stream01CompatExt as _};
